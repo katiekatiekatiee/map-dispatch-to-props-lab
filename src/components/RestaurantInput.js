ded89a1a818b9d121f45793e3558d9a9a4249043
Catherine Hodgkinson <catherinemhodgkinson@gmail.com>
2021-11-22 21:13:29 -0500

    commit tree 876e2ab18c559ac485ba5d6f45a81a6f1e2395ab

--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -24,7 +24,10 @@ export class RestaurantInput extends Component {
   handleOnSubmit = event => {
     event.preventDefault();
     // add missing code
+    this.props.addRestaurant(this.state)
   }
+
+
 
   render() {
     return(
@@ -49,6 +52,21 @@ export class RestaurantInput extends Component {
   }
 };
 
+  // const mapDispatchToProps = dispatch => {
+  //   return {
+  //     addTodo: (todo) => {
+  //       dispatch(addTodo(todo))
+  //     }
+  //   };
+  // };
+
+  const mapDispatchToProps = dispatch => {
+    return {
+      addRestaurant: (restaurant) => {
+        dispatch(addRestaurant(restaurant))
+      }
+    }
+  }
 
 //connect this component by wrapping RestaurantInput below
-export default RestaurantInput
+export default connect(null, mapDispatchToProps)(RestaurantInput)
